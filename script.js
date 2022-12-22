f21c7f1401e1c7bc0bef50a0a60199be9be0dd40
Juliano Sill <sill.juliano@gmail.com>
2022-12-21 22:53:36 -0300

    feat(buttons): add EventListener to each button

--- script.js
+++ script.js
@@ -94,3 +94,32 @@ class Calculator {
 };
 
 const calculator = new Calculator(previousTextElement, currentTextElement);
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+operatorButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+equalsButton.addEventListener('click', button => {
+	calculator.compute();
+	calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+	calculator.clear();
+	calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+	calculator.delete();
+	calculator.updateDisplay();
+});
