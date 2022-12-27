7b4bafd7855ce7927a391c19da75df82d9387bcd
Juliano Sill <sill.juliano@gmail.com>
2022-12-27 20:44:26 -0300

    chore: add and update comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Global variables
-// Use [] to select dataset itself (not its value)
+// Use [] to select data-* itself (not its value)
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('[data-equals');
@@ -13,7 +13,7 @@ class Calculator {
 	constructor(previousTextElement, currentTextElement) {
 		this.previousTextElement = previousTextElement;
 		this.currentTextElement = currentTextElement;
-		// .clear() method removes all elements from a object
+		// .clear() method removes all elements from the object
 		this.clear();
 	}
 
@@ -26,43 +26,43 @@ class Calculator {
 
 	// Removes the last digit of the current number each time the function delete() is called
 	delete() {
-		// .toString() transforms the number in a string. This enables to cut the last digit using .slice().
-		// .slice(0, -1) selects the first number (index: 0) and the first number before the last one (index: last - 1). Then store this new number in this.currentNumber.
+		// .toString() transforms the number to string. This enables to cut the last digit using .slice().
+		// .slice(0, -1) selects the first number (index: 0) and the second-to-last number (index: last - 1). Then store this new number in "this.currentNumber".
 		this.currentNumber = this.currentNumber.toString().slice(0, -1);
 	}
 
-	// Adds a number to the display
+	// Adds a number to the display when the button is clicked. The parameter (number) refers to the button's number value that is taken by event listener.
 	appendNumber(number) {
 		// If it already exists a '.' (decimal) then returns and doesn't add another '.'
 		if(number === '.' && this.currentNumber.includes('.')) return;
-		// Converts the currentNumber to string before appending the new number to avoid summing the new number to the currentNumber
+		// Converts the "currentNumber" to string before appending the new number to avoid adding it to the "currentNumber"
 		this.currentNumber = this.currentNumber.toString() + number.toString();
 	}
 
 	// Chooses the operator for computing
 	chooseOperation(operator) {
-		// If the currentNumber is empty when the operator is chosen, does nothing
+		// If the "currentNumber" is empty when the operator is chosen, does nothing
 		if(this.currentNumber === '') return;
-		// If the previousNumber is not empty, computes the previousNumber with the currentNumber
+		// If the "previousNumber" is not empty, computes the "previousNumber" with the "currentNumber"
 		if(this.previousNumber !== '') {
 			this.compute();
 		}
 		this.operator = operator;
-		// After the computing, passes the value of currentNumber to previousNumber
+		// After the computing, passes the value of "currentNumber" to "previousNumber"
 		this.previousNumber = this.currentNumber;
-		// Then resets the value of currentNumber
+		// Then resets the value of "currentNumber"
 		this.currentNumber = '';
 	}
 
 	// Computes the numbers
 	compute() {
-		// Declare a variable to store the result of the computation
+		// Declares a variable to store the result of the computation
 		let computation;
-		// parseFloat converts strings to numbers
+		// parseFloat() accepts the string and converts it into a floating-point number
 		const prev = parseFloat(this.previousNumber);
 		const current = parseFloat(this.currentNumber);
 		
-		// If the previousNumber or currentNumber is not a number, does nothing
+		// If the "previousNumber" or "currentNumber" is not a number (isNaN), does nothing
 		if(isNaN(prev) || isNaN(current)) return;
 
 		// Computes the numbers according to the chosen operator
@@ -83,31 +83,38 @@ class Calculator {
 				return;
 		};
 
-		// Updates the currentNumber with the result of the computation
+		// Updates the "currentNumber" value with the result of the computation
 		this.currentNumber = computation;
-		// Resets the operator and previousNumber
+		// Resets the operator and "previousNumber"
 		this.operator = undefined;
 		this.previousNumber = '';
 	}
 
 	getDisplayNumber(number) {
+		// Converts the number to string
 		const stringNumber = number.toString();
+		// "stringNumber.split('.')[0]" splits the number in two elements (one before and one after "."), takes the first one and then stores as a number using parseFlot()
 		const integerDigits = parseFloat(stringNumber.split('.')[0]);
+		// Splits the number in two elements (before and after ".") and stores the second (after ".")
 		const decimalDigits = stringNumber.split('.')[1];
 		let integerDisplay;
+		// If the "integerDigits" is not a number, sets as empty
 		if(isNaN(integerDigits)) {
 			integerDisplay = '';
 		} else {
+			// ".toLocaleString()" formats the number according to the language selected (in this case, "en"/english - example: 1,000 instead of 1000)
+			// "maximumFractionDigits" option ignores any number after "." (decimal)
 			integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
 		};
 		if(decimalDigits != null) {
+			// If the number has decimals, concatenates "integerDisplay" and "decimalDigits"
 			return `${integerDisplay}.${decimalDigits}`;
 		} else {
 			return integerDisplay;
 		}
 	}
 
-	// Updates the number in the display each time a button is clicked
+	// Updates the number in the display each time the button is clicked
 	updateDisplay() {
 		// Runs the getDisplayNumber() and updates the currentTextElement accordingly (example: 1000 changes to 1,000)
 		this.currentTextElement.innerText = this.getDisplayNumber(this.currentNumber);
@@ -116,17 +123,25 @@ class Calculator {
 	}
 };
 
+// Creates a new instance in Calculator class with previous and current numbers as its parameters
 const calculator = new Calculator(previousTextElement, currentTextElement);
 
+
+// Add Event Listeners
+
+// Iterates each number button
 numberButtons.forEach(button => {
 	button.addEventListener('click', () => {
+		// Calls appendNumber() with the respective button's value (number) as a parameter
 		calculator.appendNumber(button.innerText);
 		calculator.updateDisplay();
 	});
 });
 
+// Iterates each operator button
 operatorButtons.forEach(button => {
 	button.addEventListener('click', () => {
+		// Calls chooseOperation() with the respective button's value (operator) as a parameter
 		calculator.chooseOperation(button.innerText);
 		calculator.updateDisplay();
 	});
